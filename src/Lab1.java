6be74e5c39a25fbb5bde61ca81a9940a527f1129
jonmi <jonmi@192.168.0.21>
2018-08-19 10:10:50 -0700

    Initial Lab Commit

---
+++ src/Lab1.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author YOUR NAME HERE
+ * 
+ * 
+ */
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
+		System.out.println("Hello, World!");
+	}
+
+}
