4c761fcd9afb8f340d1713d018ff3cd870f53d33
poseys <poseys@dhcp-10-134-210-236.uawifi.arizona.edu>
2019-01-16 08:31:18 -0700

    Changed "Hello World statment on line 11 to "Hello CSC335, I'm Aaron Posey"

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,6 @@
 /**
  * 
- * @author YOUR NAME HERE
+ * @author Aaron Posey
  * 
  * 
  */
@@ -8,7 +8,7 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello CSC 335, I'm Aaron Posey");
 	}
 
 }
